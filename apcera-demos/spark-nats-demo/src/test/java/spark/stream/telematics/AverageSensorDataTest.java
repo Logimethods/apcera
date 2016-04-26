9e83fa4932565afee4d99f1cebf15f5815403a4a
Laurent Magnin <laugimethods@Huard.local>
2016-04-26 15:18:06 -0400

    AverageSensorDataTest skeleton

---
+++ apcera-demos/spark-nats-demo/src/test/java/spark/stream/telematics/AverageSensorDataTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package spark.stream.telematics;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author laugimethods
+ *
+ */
+public class AverageSensorDataTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link spark.stream.telematics.AverageSensorData#computeAvgFromStream(org.apache.spark.streaming.api.java.JavaPairInputDStream)}.
+	 */
+	@Test
+	public void testComputeAvgFromStream() {
+		//fail("Not yet implemented");
+	}
+
+}
