12271637c4648bfa1daee2a5c65d30453d694703
Laurent Magnin <laugimethods@Huard.local>
2016-04-27 15:51:22 -0400

    src/test/resources/log4j.properties

--- apcera-demos/spark-nats-demo/src/test/java/spark/stream/telematics/AverageSensorDataTest.java
+++ apcera-demos/spark-nats-demo/src/test/java/spark/stream/telematics/AverageSensorDataTest.java
@@ -3,19 +3,17 @@
  */
 package spark.stream.telematics;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.LinkedList;
 import java.util.List;
 
-import org.apache.spark.Accumulator;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.streaming.Duration;
-import org.apache.spark.streaming.Time;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaPairInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
@@ -38,8 +36,8 @@ public class AverageSensorDataTest {
 	@SuppressWarnings("serial")
 	@Test
 	public void testComputeAvgFromStream() throws InterruptedException {
-//		SparkConf sparkConf = new SparkConf().setAppName("My Spark Job").setMaster("local");
-		JavaStreamingContext ssc = new JavaStreamingContext("local[2]", "AverageSensorDataTest", new Duration(2000));
+		SparkConf sparkConf = new SparkConf().setAppName("My Spark Job").setMaster("local[2]");
+		JavaStreamingContext ssc = new JavaStreamingContext(sparkConf, new Duration(2000));
 		try {
 			// @see http://apache-spark-user-list.1001560.n3.nabble.com/Creating-in-memory-JavaPairInputDStream-for-testing-td23956.html
 			List<Tuple2<String, String>> list = new LinkedList<Tuple2<String, String>>();
