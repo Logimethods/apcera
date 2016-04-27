4db1eb748b1ce2729b3a3d6118e31cf5e6d183dc
Laurent Magnin <laugimethods@Huard.local>
2016-04-27 10:33:58 -0400

    avgCounts.print();
    testComputeAvgFromStream() does generate avgCounts

--- apcera-demos/spark-nats-demo/src/test/java/spark/stream/telematics/AverageSensorDataTest.java
+++ apcera-demos/spark-nats-demo/src/test/java/spark/stream/telematics/AverageSensorDataTest.java
@@ -33,34 +33,32 @@ public class AverageSensorDataTest {
 
 	/**
 	 * Test method for {@link spark.stream.telematics.AverageSensorData#computeAvgFromStream(org.apache.spark.streaming.api.java.JavaPairInputDStream)}.
+	 * @throws InterruptedException 
 	 */
 	@Test
-	public void testComputeAvgFromStream() {
+	public void testComputeAvgFromStream() throws InterruptedException {
 //		SparkConf sparkConf = new SparkConf().setAppName("My Spark Job").setMaster("local");
 		JavaStreamingContext ssc = new JavaStreamingContext("local[2]", "AverageSensorDataTest", new Duration(2000));
-		// @see http://apache-spark-user-list.1001560.n3.nabble.com/Creating-in-memory-JavaPairInputDStream-for-testing-td23956.html
-		List<Tuple2<String, String>> list = new LinkedList<Tuple2<String, String>>(); 
-		
-		Tuple2<String, String> tupple = new Tuple2<String, String>("1", "112"); 
-		list.add(tupple); 
-		
-		tupple = new Tuple2<String, String>("1", "115"); 
-		list.add(tupple); 
-		
-		JavaRDD<Tuple2<String, String>> rdd = ssc.sparkContext().parallelize(list); 
-		rdd.cache();
-//		System.out.println(rdd.collect());
-		assertEquals(2, rdd.count());
-		
-		JavaPairInputDStream<String, String> stackStream = Helpers.createJavaPairInputDStream(ssc, rdd); 
-		stackStream.cache();
-		
-		Time time = new Time(4000);
-//		System.out.println(stackStream.compute(time).collect());
-		assertEquals(2, stackStream.compute(time).count());
-		
-		JavaPairDStream<String, AvgCount> avgCounts = AverageSensorData.computeAvgFromStream(stackStream);
-//		System.out.println(avgCounts.compute(time).collect());
+		try {
+			// @see http://apache-spark-user-list.1001560.n3.nabble.com/Creating-in-memory-JavaPairInputDStream-for-testing-td23956.html
+			List<Tuple2<String, String>> list = new LinkedList<Tuple2<String, String>>();
+			Tuple2<String, String> tupple = new Tuple2<String, String>("1", "Temperature=25");
+			list.add(tupple);
+			tupple = new Tuple2<String, String>("1", "Temperature=25");
+			list.add(tupple);
+			JavaRDD<Tuple2<String, String>> rdd = ssc.sparkContext().parallelize(list);
+			JavaPairInputDStream<String, String> stackStream = Helpers.createJavaPairInputDStream(ssc, rdd);
+		    
+			JavaPairDStream<String, AvgCount> avgCounts = AverageSensorData.computeAvgFromStream(stackStream);
+
+			avgCounts.print();
+			
+		    ssc.start();
+		    Thread.sleep(5000);					
+		    ssc.stop();
+		} finally {
+			ssc.stop();
+		}
 	}
 
 }
